a74f17d220557f2f99eba3d6e9d2dec9bdf4c3d0
danywizzz <razumnyyd@gmail.com>
2022-07-04 17:45:12 +1000

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  Math.sign(n) === 1 ? n : n *= -1
+  let str = n.toString();
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  let reverseNumber = Number(reverseStr);
+  return reverseNumber;
 }
